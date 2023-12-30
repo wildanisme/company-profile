15faf4a08a946e17b79b97ab1d9cf09904d9e70b
wildanisme <wildantafufiq.abdulaziz@gmail.com>
2023-12-31 02:22:34 +0700

    adjust comment section

--- Modules/Article/Resources/views/frontend/posts/index.blade.php
+++ Modules/Article/Resources/views/frontend/posts/index.blade.php
@@ -3,23 +3,7 @@
 @section('title') {{ __("Posts") }} @endsection
 
 @section('content')
-
-{{-- <section class="bg-gray-100 text-gray-600 py-20">
-    <div class="container mx-auto flex px-5 items-center justify-center flex-col">
-        <div class="text-center lg:w-2/3 w-full">
-            <h1 class="text-3xl sm:text-4xl mb-4 font-medium text-gray-800">
-                {{ __("Articles") }}
-            </h1>
-            <p class="mb-8 leading-relaxed">
-                We publish articles on a number of topics.
-                <br>
-                We encourage you to read our posts and let us know your feedback. It would be really help us to move forward.
-            </p>
-
-            @include('frontend.includes.messages')
-        </div>
-    </div>
-</section>
+{{--
 
 <section class="bg-white text-gray-600 p-6 sm:p-20">
     <div class="grid grid-cols-1 sm:grid-cols-3 gap-6">
@@ -65,6 +49,7 @@
         {{$$module_name->links()}}
     </div>
 </section> --}}
+
     <section class="bg-gray-100 text-gray-600 py-20">
         <div class="container mx-auto flex px-5 items-center justify-center flex-col">
             <div class="text-center lg:w-2/3 w-full">
